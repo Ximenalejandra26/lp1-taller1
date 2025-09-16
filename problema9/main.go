2cece0452c6424eb6a4b21e65697f97767186aa8
Ximena <Ximena.aleja021@outlook.com>
2025-09-15 20:34:16 -0500

    solucion problema 9

--- problema9/main.go
+++ problema9/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
@@ -10,26 +11,39 @@ import (
 // Hay 5 filósofos y 5 tenedores (recursos). Cada filósofo necesita 2 tenedores para comer.
 // Estrategia segura: imponer un **orden global** al tomar los tenedores (primero el menor ID, luego el mayor)
 // para evitar deadlock. También puedes limitar concurrencia (ej. mayordomo).
-// TODO: completa la lógica de toma/soltado de tenedores y bucle de pensar/comer.
+// completa la lógica de toma/soltado de tenedores y bucle de pensar/comer.
 
 type tenedor struct{ mu sync.Mutex }
 
 func filosofo(id int, izq, der *tenedor, wg *sync.WaitGroup) {
-	// TODO: desarrolla el código para el filósofo
-	
+	defer wg.Done()
+	// desarrolla el código para el filósofo
+	for i := 0; i < 3; i++ { // cada filósofo come 3 veces
+		pensar(id)
+		if id == 4 {	
+				der.mu.Lock()
+				izq.mu.Lock()
+		} else {
+				izq.mu.Lock()
+				der.mu.Lock()
+		}
+		comer(id)
+		izq.mu.Unlock()
+		der.mu.Unlock()
+	}
 	fmt.Printf("[filósofo %d] satisfecho\n", id)
 }
 
 func pensar(id int) {
 	fmt.Printf("[filósofo %d] pensando...\n", id)
-	// TODO: simular tiempo de pensar
-
+	//  simular tiempo de pensar
+	time.Sleep(time.Duration(rand.Intn(200)+50) * time.Millisecond)
 }
 
 func comer(id int) {
 	fmt.Printf("[filósofo %d] COMIENDO\n", id)
-	// TODO: simular tiempo de pensar
-
+	//  simular tiempo de pensar
+	time.Sleep(time.Duration(rand.Intn(200)+50) * time.Millisecond)
 }
 
 func main() {
@@ -40,16 +54,16 @@ func main() {
 	// crear tenedores
 	forks := make([]*tenedor, n)
 	for i := 0; i < n; i++ {
-		// TODO: inicializar cada tenedor i
-
+		//  inicializar cada tenedor i
+		forks[i] = &tenedor{}
 	}
 
 	// lanzar filósofos
 	for i := 0; i < n; i++ {
 		izq := forks[i]
 		der := forks[(i+1)%n]
-		// TODO: lanzar goroutine para el filósofo i
-
+		// lanzar goroutine para el filósofo i
+		go filosofo(i, izq, der, &wg)
 	}
 
 	wg.Wait()
